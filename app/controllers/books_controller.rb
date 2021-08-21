0a40a06bfd435bd349f95bf32258a0aa078a7b62
usskr <rei.takagi731@gmail.com>
2021-08-21 13:31:28 +0000

    [Fix] 新規投稿後のリダイレクト先、ログイン後の遷移先、他ユーザの投稿編集ができなかった際のリンク先、users/indexのレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
     if @book.user == current_user
       render "edit"
     else
-      redirect_to book_path(@book)
+      redirect_to books_path
     end
   end
 
